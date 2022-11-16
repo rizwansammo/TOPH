4905698b1e681ef2c6676a82bc454e4f7e2a70f8
Rizwan Sammo <mrizwan.sammo@gmail.com>
2022-11-16 21:21:03 +0600

    sin Function

---
+++ sinFunction.py
@@ -0,0 +1,10 @@
+# Python code to demonstrate the working of sin()
+     
+# importing "math" for mathematical operations 
+import math 
+    
+a = math.pi / 6
+     
+# returning the value of sine of pi / 6 
+print ("The value of sine of pi / 6 is : ", end ="") 
+print (math.sin(a)) 
