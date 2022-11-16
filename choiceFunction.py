3134f7b708e51c415030e577b331442a47a6c4ad
Rizwan Sammo <mrizwan.sammo@gmail.com>
2022-11-16 21:25:23 +0600

    Choice Function

---
+++ choiceFunction.py
@@ -0,0 +1,10 @@
+# import random 
+import random
+  
+# prints a random value from the list
+list1 = [1, 2, 3, 4, 5, 6] 
+print(random.choice(list1))
+  
+# prints a random item from the string 
+string = "striver" 
+print(random.choice(string))  
